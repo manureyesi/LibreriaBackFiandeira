17e549adf44270b8344f44539ef5a8bb90627e26
manureyesi <manureyesi@outlook.es>
2019-05-20 23:49:49 +0200

    Rangos

---
+++ src/es/fiandeira/rangos/models/Rangos.java
@@ -0,0 +1,14 @@
+package es.fiandeira.rangos.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "RANGOS")
+public class Rangos implements Serializable {
+
+	
+	
+}
---
+++ src/es/fiandeira/rangos/models/RangosPK.java
@@ -0,0 +1,5 @@
+package es.fiandeira.rangos.models;
+
+public class RangosPK {
+
+}
